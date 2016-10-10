c12b289df70abcda794ef1a2ca31daea4b760714
Karuna Lingham <karuna.lingham@codepandora.com>
2016-10-10 20:07:21 +0530

    Setup the test suite for User Web app

---
+++ 01_SignUp/1_1_incorrect_signup.js
@@ -0,0 +1,5 @@
+describe("Sign Up: Incorrect", function () {
+
+  
+
+});
---
+++ sminq_tests.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+echo '''
+Copyright (C) 2016 Sminq India Solutions Pvt Ltd.
+Created on 2016-10-10
+Updated on 2016-10-10
+
+ ____    __  __   ___   _   _    ___
+/ ___|  |  \/  | |_ _| | \ | |  / _ \
+\___ \  | |\/| |  | |  |  \| | | | | |
+___) |  | |  | |  | |  | |\  | | |_| |
+|____/  |_|  |_| |___| |_| \_|  \__\_\
+
+
+@author: Karuna Lingham
+'''
+
+echo "================================="
+echo "Sminq User Web v1.0.0 ..."
+echo "Running Test Suite v1.0.0 ..."
+echo "================================="
+
+d="$(date +'%d-%m-%Y')"
+t="$(date +%H:%M)"
+now=$d-$t
+
+start=`date +%s`
+
+#Run scripts
+protractor sminq_conf.js
+# python add_sminq.py
+
+#Copy ./reports folder
+mkdir -p test_reports/test_$now
+cp reports/htmlReport.html test_reports/test_$now/report.html
+cp -R reports/screenshots test_reports/test_$now
+
+end=`date +%s`
+total_time=$((end-start))
+
+echo "================================="
+echo "Time taken for sminq test suite: $total_time sec."
+echo "================================="
