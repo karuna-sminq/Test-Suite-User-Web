a0dbe543e3a75f953812f1bdd72136bdee249308
Karuna Lingham <karuna.lingham@codepandora.com>
2016-10-15 19:48:23 +0530

    Added test file for Keyword search

--- 03_Home/3_2_doctor_search.js
+++ 03_Home/3_2_doctor_search.js
@@ -1,8 +1,8 @@
 describe("Home Page:", function() {
 
-    it("Doctor/Clinic Name Search should search through", function() {
+  var sleep_time = 3000;
 
-      var sleep_time = 3000;
+    it("Doctor/Clinic Name Search should search through", function() {
 
       browser.get("http://localhost:9000/#/");
 
@@ -20,7 +20,26 @@ describe("Home Page:", function() {
       //Check for Doctor name searched
       expect(element(by.xpath('/html/body/div[2]/div/div/div[2]/div[2]/span[2]')).getText()).toContain(doctor);
 
+      browser.navigate().back();
+
     });
 
+    it("Keyword Search should search through", function () {
+
+      var keyword = "OPD";
+
+      //Enter doctor name to search
+      element(by.model('search.keyword')).sendKeys(keyword);
+
+      browser.sleep(sleep_time);
+
+      element(by.id('search-input-button')).click();
+
+      browser.sleep(sleep_time);
+
+      //Check for Doctor name searched
+      expect(element(by.xpath('/html/body/div[2]/div/div/div[2]/div[2]/span[2]')).getText()).toContain(keyword);
+
+    });
 
 });
