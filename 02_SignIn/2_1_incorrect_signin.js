8012291cf873f19c1200cb99913b366583676114
Karuna Lingham <karuna.lingham@codepandora.com>
2016-11-25 12:16:27 +0530

    Modified Incorrect Sign in test script

--- 02_SignIn/2_1_incorrect_signin.js
+++ 02_SignIn/2_1_incorrect_signin.js
@@ -4,39 +4,37 @@ describe("Sign In: Incorrect", function () {
 
   it("New user should not be able to sign in without signing up first", function () {
 
-      // browser.get("http://localhost:9000/#/");
+      browser.get("http://localhost:9000");
+
+      browser.sleep(sleep_time);
+
+      element(by.css('a[href="/login?type=login"]')).click();
+
+      browser.sleep(sleep_time);
+
+      // //Sign In Tab
+      // element(by.xpath('/html/body/div/div[2]/div/div/div[2]/div/ul/li[2]/a')).click();
       //
       // browser.sleep(sleep_time);
 
-      element(by.css('a[href="#/login"]')).click();
-
-      browser.sleep(sleep_time);
-
-      //Sign In Tab
-      element(by.xpath('/html/body/div/div[2]/div/div/div[2]/div/ul/li[2]/a')).click();
-
-      browser.sleep(sleep_time);
-
       var mobile = 7242234200;
 
-      var pin = 1111;
-
-      var login_btn = element(by.xpath('/html/body/div/div[2]/div/div/div[2]/div/div/div[2]/div/form/div[3]/button'));
+      var login_btn = element(by.id('sign-in-button'));
 
       expect(login_btn.isEnabled()).toBeFalsy();
 
-      element(by.xpath('/html/body/div/div[2]/div/div/div[2]/div/div/div[2]/div/form/div[1]/input')).sendKeys(mobile);
+      element(by.xpath('/html/body/div[1]/div[2]/div/div/div[2]/div/div/div[2]/div/form/div[1]/input')).sendKeys(mobile);
 
-      element(by.xpath('/html/body/div/div[2]/div/div/div[2]/div/div/div[2]/div/form/div[2]/input')).sendKeys(pin);
+      browser.sleep(sleep_time);
 
       //Valid Fields -> Button should be enabled
       expect(login_btn.isEnabled()).toBeTruthy();
 
       login_btn.click();
 
-      expect(element(by.id('toast-container')).getText()).toContain('does not exist');
-
       browser.sleep(sleep_time);
+
+      expect(element(by.id('toast-container')).getText()).toContain('DOES NOT EXIST');
 
   });
 
--- sminq_conf.js
+++ sminq_conf.js
@@ -10,10 +10,10 @@ exports.config = {
         defaultTimeoutInterval: 2500000
     },
     specs: [
-        '01_SignUp/1_1_incorrect_signup.js',
-        '01_SignUp/1_2_correct_signup.js',
-        '01_SignUp/1_3_logout.js',
-        // '02_SignIn/2_1_incorrect_signin.js',
+        // '01_SignUp/1_1_incorrect_signup.js',
+        // '01_SignUp/1_2_correct_signup.js',
+        // '01_SignUp/1_3_logout.js',
+        '02_SignIn/2_1_incorrect_signin.js',
         // '02_SignIn/2_2_correct_signin.js',
         // '03_Home/3_1_locality_search.js',
         // '03_Home/3_2_doctor_search.js',
